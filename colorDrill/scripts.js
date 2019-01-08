8c508b5ca37e298bd469d9214c214e45de9fde26
TravisPRyan <43801485+TravisPRyan@users.noreply.github.com>
2019-01-08 13:35:22 -0500

    event listener
    Adding event listener and logic comparing a clicked cubes color value to initial value – with reporting.

--- colorDrill/scripts.js
+++ colorDrill/scripts.js
@@ -12,7 +12,24 @@ var colorsArr = [
 
 //loop through cube divs and assign the color values from the colorsaArr to each div
 var cubes = document.querySelectorAll(".cube");
+//setting up initial value for color to be used as target
+var initialSel = colorsArr[3];
+//tageted through span id (not class) colorSel
+var colorSelect = document.querySelector("#colorSel");
+colorSelect.textContent = initialSel;
 
 for(var i = 0; i < cubes.length; i++){
+	//add initial colors to cubes
 	cubes[i].style.backgroundColor = colorsArr[i];
+	//add click listeners to cubes
+	cubes[i].addEventListener('click', function(){
+		//identify color of clicked cube and compare it to initialSel
+		clickedCube = this.style.backgroundColor;
+		if(clickedCube === initialSel){
+			alert("CORRECT!");
+		} else{
+			alert("INCORRECT");
+		}
+
+	});
 }
